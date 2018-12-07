6b15d8522b831ef6d39440bfc5289898cfc4bd44
Andrew Wonnacott <awonnacott@princeton.edu>
2018-12-07 15:41:09 -0500

    Annotation with silly symbols

--- lawi.py
+++ lawi.py
@@ -43,14 +43,15 @@ class UnwindingVertex:
     def __init__(self, parent, transition, location):
         self.parent = parent
         self.parent.children.add(self)
-        self.transition = transition
-        self.location = location
+        self.transition = transition  # \( T \)
+        self.location = location  # \( M_v(self) \)
+        # \( M_e(self.parent, self) \)
         self.children = set()
-        self.label = True
+        self.label = True  # \( \psi(self) \)
         self.num = UnwindingVertex.next_num
         UnwindingVertex.next_num += 1
 
-    def __lt__(self, other):
+    def __lt__(self, other):  # \( \prec \)
         return self.num < other.num
 
     def __le__(self, other):
@@ -68,7 +69,7 @@ class UnwindingVertex:
     def __ne__(self, other):
         return self.num != other.num
 
-    def has_weak_ancestor(self, other):
+    def has_weak_ancestor(self, other):  # \( self \sqsubseteq other \)
         return self == other or (self.parent is not None and self.parent.has_weak_ancestor(other))
 
     def ancestors_path(self):
@@ -81,11 +82,23 @@ class UnwindingVertex:
 
 
 class Unwinding:
-    def __init__(self, cfa):
-        self.verts = set(UnwindingVertex(None, None, None))
-        self.uncovered_verts = set(self.verts)
-        self.covering = set()
-        while self.uncovered_verts:
+    def __init__(self, cfa, loc_entry, loc_exit):
+        self.loc_entry = loc_entry  # \( l_i \)
+        self.loc_exit = loc_exit  # \( l_f \)
+        eps = UnwindingVertex(
+            location=loc_entry,
+            parent=None,
+            transition=None,
+        )
+        self.verts = {eps}  # \( V \leftarrow \{ \epsilon \} \)
+        # \( E \) is stored as successor lists on vertices
+        # \( \psi \) is stored as labels on vertices
+        self.covering = set()  # \( \triangleright \)
+        # self.uncovered_verts caches uncovered vertices
+        # \( \epsilon \) is initially uncovered
+        self.uncovered_verts = {eps}
+        self.cfa = cfa  # cfa.verts is \( \Lambda \)
+        while self.uncovered_verts:  # TODO: only take uncovered leaves
             v = self.uncovered_verts.pop()
             w = v.parent
             while w is not None:
@@ -95,14 +108,14 @@ class Unwinding:
 
     def close(self, v):
         for w in self.verts:
-            if w < v and self.M(v)(w) == self.M(v)(v):
+            if w < v and w.location == v.location:
                 self.cover(v, w)
 
     def dfs(self, v):
         self.close(v)
         if v not in self.uncovered_verts:
             return
-        if self.M(v)(v) == self.l_f:
+        if v.location == self.loc_entry:
             self.refine(v)
             w = v
             while w is not None:
@@ -113,47 +126,46 @@ class Unwinding:
                 self.dfs(w)
 
     def cover(self, v, w):
-        if v in self.uncovered_verts and self.M(v)(v) == self.M(v)(w) and not w.has_weak_ancestor(v):
-            if v.label.models(w.label):
+        if v in self.uncovered_verts and v.location == w.location and not w.has_weak_ancestor(v):
+            if v.label.models(w.label):  # TODO: models (use z3)
                 self.covering.add((v, w))
                 self.covering = set(
                     (x, y)
                     for (x, y) in self.covering
                     if not y.has_weak_ancestor(v)
                 )
+                # TODO: regenerate self.uncovered_verts
 
     def refine(self, v):
-        if self.M(v)(v) == self.l_f and v.label.satisfiable():
+        if v.location == self.loc_exit and v.label.models(False):  # TODO: models (use z3)
             v_pi, u_pi = v.ancestors_path()
-            u_pi = [t.timeshift(i) for (i, t) in enumerate(u_pi)]
+            u_pi = [t.timeshift(i) for (i, t) in enumerate(u_pi)]  # TODO: timeshift (wtf is it)
             assert(len(v_pi) == len(u_pi) + 1)
-            a_hat = make_interpolant(u_pi)
+            # make_interpolant aborts if no interpolant exists
+            # TODO: catch make_interpolant expcetion and repackage it with current state
+            a_hat = make_interpolant(u_pi)  # TODO: make_interpolant (how?)
             assert(len(a_hat) == len(v_pi))
             for i in range(len(a_hat)):
-                phi = a_hat[i].timeshift(-i)
-                if v_pi[i].label.derives(phi):
+                phi = a_hat[i].timeshift(-i)  # TODO: timeshift
+                if not v_pi[i].label.models(phi):  # TODO: models (use z3)
                     self.covering = set(
                         (x, y)
                         for (x, y) in self.covering
                         if y == v_pi[i]
                     )
-                    v_pi[i].label &= phi
+                    # TODO: regnerate self.uncovered_verts
+                    v_pi[i].label &= phi  # TODO: &= for labels (use z3)
 
-    def expand(self, cfa, v):
+    def expand(self, v):
         if v in self.uncovered_verts and v.children:
-            for (m_v_v, t, m) in cfa.succs[v.location]:
-                self.verts.add(UnwindingVertex(v, t, m))
-                self.m(v)[w] = m
-                self.m(e)[(v, w)] = True
-
-
-def analyze(domain, cfa):
-    """Given a domain and a control flow automaton, compute the least
-    inductive annotation (i.e., for all edge (u,v), we have
-    domain.leq(annotation[u].post(cfa.command(u,v)), annotation[v])) such that
-    annotation[cfa.entry] is domain.top
-    """
-    return {}
+            for m in cfa.succs[v.location]:  # TODO: really, all of them?
+                w = UnwindingVertex(
+                    parent=v,
+                    transition=self.cfa.command(v.location, m),
+                    location=m,
+                )
+                self.verts.add(w)
+                self.uncovered_verts.add(w)  # TODO: is this correct?
 
 
 def analyze_and_print(domain, stmt):
@@ -161,6 +173,6 @@ def analyze_and_print(domain, stmt):
     loc_entry = cfa.fresh_vertex()
     loc_exit = cfa.fresh_vertex()
     stmt.to_cfa(cfa, loc_entry, loc_exit)
-    annotation = analyze(domain, cfa)
+    annotation = Unwinding(cfa, loc_entry, loc_exit)
     stmt.print_annotation(annotation, 0)
     print("{" + str(annotation[loc_exit]) + "}")
